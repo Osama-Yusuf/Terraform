82e29f6f5dc1cb9be28c9d5489dedbda735aa25b
osama <osama9mohamed5@gmail.com>
2021-12-24 21:52:28 +0200

    Defined initial web security group

--- prod.tf
+++ prod.tf
@@ -1,14 +1,41 @@
 provider "aws" {
-    profile = "default"
-    region  = "us-west-2"
+  profile = "default"
+  region  = "us-west-2"
 }
 
 resource "aws_s3_bucket" "prod_tf_course" { 
-    bucket = "tf-course-osama-2022"
-    acl    = "private"
+  bucket = "tf-course-osama-2022"
+  acl    = "private"
 }
 
 resource "aws_default_vpc" "default" {}
+
+resource "aws_security_group" "prod_web" {
+  name        = "prod_web"
+  description = "Allow standard http and https ports inbount and all outbound"
+  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    "Terraform" : "true"
+  }
+}
 
 # resource "aws_instance" "ubuntu" {
 #     ami           = "ami-830c94e3"
