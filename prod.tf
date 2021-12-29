21833819330c797859ab1927919dd9bf613beadd
osama <osama9mohamed5@gmail.com>
2021-12-29 17:13:59 +0200

    split data out to tfvars

--- prod.tf
+++ prod.tf
@@ -1,3 +1,79 @@
+variable "whitelist" {
+  type = list(string)
+}
+variable "web_image_id" {
+  type = string
+}
+variable "web_instance_type" {
+  type = string
+}
+variable "web_desired_capacity" {
+  type = number
+}
+variable "web_max_size" {
+  type = number
+}
+variable "web_min_size" {
+  type = number
+}
+
+
+/* -------------------------------- variables ------------------------------- */ # Variables are not that handy they are good for learning
+
+# variable "web_ami" {
+  # type  = string
+  # default = "ami-abc123"
+# }
+
+# resource "aws_instance" "web" {
+  # ami           = var.web_ami  # or "${var.web_ami}"
+  # instance_type = "t2.micro"
+# }
+
+# --- as you can override the var value in the command line like this:
+# --- terraform apply -var web_ami=ami-def456
+
+/* -------------------------------- variables ------------------------------- */
+
+/* ---------------------------------------------------------------------------------------------------------------- */
+
+/* ------------------------------ Data Sources ------------------------------ */  # Data Sources are like variables, but they retrive data from AWS or other terraform configuration
+
+# --------------------------------- Examble One --------------------------------- 
+
+# data "aws_ami" "web_ami" {
+  # most_recent = true
+  # owners      = ["self"]
+
+  # tags = {
+    # Name   = "webserver"
+    # Deploy = "true"
+  # }
+# }
+
+# resource "aws_instance" "web" {
+  # ami = data.aws_ami.web_ami.id
+# }
+
+# --------------------------------- Examble Two --------------------------------- 
+
+# data "aws_ami" "ubuntu" {
+  # most_recent = true
+  # filter {
+    # name   = "name"
+    # values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  # }
+  # filter {
+    # name   = "virtualization-type"
+    # values = ["hvm"]
+  # }
+  # owners      = ["099720109477"] # 099720109477 is Canonical
+# }
+
+/* ------------------------------ Data Sources ------------------------------ */
+
+/* ---------------------------------------------------------------------------------------------------------------- */
+
 provider "aws" {
   profile = "default"
   region  = "us-west-2"
@@ -34,14 +110,14 @@ resource "aws_security_group" "prod_web" {
   ingress {
     from_port   = 80
     to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp" 
+    cidr_blocks = var.whitelist
   }
   ingress {
     from_port   = 443
     to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp" 
+    cidr_blocks = var.whitelist
   }
   egress {
     from_port   = 0
@@ -53,6 +129,7 @@ resource "aws_security_group" "prod_web" {
     "Terraform" : "true"
   }
 }
+
 # -------------------------- commented the next block to creaete an auto scaling group
 # resource "aws_instance" "prod_web" {
 #   count         = 2
@@ -100,16 +177,16 @@ resource "aws_elb" "prod_web" {
 
 resource "aws_launch_template" "prod_web" {
   name_prefix   = "prod-web"
-  image_id      = "ami-0f129ae03ee4c74a0"
-  instance_type = "t2.micro"
+  image_id      = var.web_image_id
+  instance_type = var.web_instance_type
 }
 
 resource "aws_autoscaling_group" "prod_web" {
   # vpc_zone_identifier = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
   availability_zones  = ["us-west-2a", "us-west-2b"]
-  desired_capacity    = 1
-  max_size            = 1
-  min_size            = 1
+  desired_capacity    = var.web_desired_capacity
+  max_size            = var.web_max_size
+  min_size            = var.web_min_size
 
   launch_template {
     id      = aws_launch_template.prod_web.id
